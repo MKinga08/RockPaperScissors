17677de133efdd4b4d3855541787666c15f36d9a
MKinga08 <dr.m.kinga08@gmail.com>
2022-11-01 13:01:55 +0100

    add main, input validator and README file

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Rock_Paper_Scissors
-First project
+
+
+## My very first project :)
+
+## Task:
+Writing a rock-paper-scissors game. If you don't know how this 
+game works you can read about the rules here: [Rock_Paper_Scissors](https://en.wikipedia.org/wiki/Rock_paper_scissors)
+
+How the game works:
+- First, you have to choose between the given objects:
+    - these are case-insensitive and
+    - you can't give an invalid input
+- Then, the computer chooses a random object too.
+- The program prints who won, and also gives you a notice about the scores
+- Finally, it asks you whether you want to play again
--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import random
 
-def get_computers_choice():
 
+def get_computers_choice():
     computers_choice = random.choice(['rock', 'paper', 'scissors'])
     return computers_choice
 
-def get_users_input():
 
-    opt = str(input('Enter rock/paper/scissors:'))
-    return opt.lower()
+def get_users_input():
+    while True:
+        opt = input('Enter rock/paper/scissors:').lower()
+        if opt == "rock" or opt == "paper" or opt == "scissors":
+            return opt
+        else:
+            print("Wrong input")
+
 
 def announcing_winner(result, computers_choice):
-
     global count_player, count_computer
     if result == computers_choice:
         print('Its a draw')
@@ -24,6 +28,7 @@ def announcing_winner(result, computers_choice):
         print("You lost")
         count_computer += 1
 
+
 def playing_again():
     while True:
         play = input('Do you want to play again?')
@@ -32,16 +37,22 @@ def playing_again():
         elif play == 'no':
             return False
 
+
 count_player = 0
 count_computer = 0
-again = True
 
 
-while again:
-    result = get_users_input()
-    print(f"Your choice is: {result}")
-    computers_choice = get_computers_choice()
-    print(f"The computers choice is: {computers_choice}")
-    announcing_winner(result, computers_choice)
-    print(f'The score of the game is: you:computer {count_player}:{count_computer}')
-    again = playing_again()
+def main():
+    again = True
+    while again:
+        result = get_users_input()
+        print(f"Your choice is: {result}")
+        computers_choice = get_computers_choice()
+        print(f"The computers choice is: {computers_choice}")
+        announcing_winner(result, computers_choice)
+        print(f'The score of the game is: you:computer {count_player}:{count_computer}')
+        again = playing_again()
+
+
+if __name__ == "__main__":
+    main()
